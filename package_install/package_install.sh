f2673fb7fc2f10c37babcc5c9332bc4c522e43e0
Noah Apthorpe <apthorpe@princeton.edu>
2018-07-20 20:41:20 -0400

    new scripts

--- package_install/package_install.sh
+++ package_install/package_install.sh
@@ -2,5 +2,5 @@
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > anaconda_install.sh
 chmod 777 anaconda_install.sh
 ./anaconda_install.sh
-source activate base
+source ~/miniconda3/bin/activate
 pip install -r requirements.txt
