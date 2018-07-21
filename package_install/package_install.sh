fb42786d3dab36c178eab59ce883d963ad031f33
Noah Apthorpe <apthorpe@princeton.edu>
2018-07-20 20:47:01 -0400

    numpy install fix

--- package_install/package_install.sh
+++ package_install/package_install.sh
@@ -2,5 +2,6 @@
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > anaconda_install.sh
 chmod 777 anaconda_install.sh
 ./anaconda_install.sh
-source ~/miniconda3/bin/activate
+source ~/miniconda3/bin/activate base
+pip install numpy
 pip install -r requirements.txt
