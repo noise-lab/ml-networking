8641dbab272498322a83a28c0769ce869891443a
Noah Apthorpe <apthorpe@princeton.edu>
2018-07-20 20:23:32 -0400

    package installs

---
+++ package_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ “$OSTYPE” == “linux-gnu” ]]; then
+    # ...
+    curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > anaconda_install.sh
+elif [[ “$OSTYPE” == “darwin”* ]]; then
+    # Mac OSX
+    curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > anaconda_install.sh
+elif [[ “$OSTYPE” == “msys” ]]; then
+    # Windows git bash
+    curl https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe > anaconda_install.sh
+else
+       echo "OS unknown"
+fi
+
+chmod 777 anaconda_install.sh
+./anaconda_install.sh
+source activate base
+pip install -r requirements.txt
