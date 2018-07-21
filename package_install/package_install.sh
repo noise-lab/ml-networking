cb7cdb170a689b555d08d2fc46030c7a7e7733d8
Noah Apthorpe <apthorpe@princeton.edu>
2018-07-20 20:33:50 -0400

    no OS detection

--- package_install/package_install.sh
+++ package_install/package_install.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    # ...
-    curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > anaconda_install.sh
-elif [[ "$OSTYPE" == "Darwin"* ]]; then
-    # Mac OSX
-    curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > anaconda_install.sh
-elif [[ "$OSTYPE" == "msys" ]]; then
-    # Windows git bash
-    curl https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe > anaconda_install.sh
-else
-       echo "OS unknown"
-fi
-
+curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > anaconda_install.sh
 chmod 777 anaconda_install.sh
 ./anaconda_install.sh
 source activate base
