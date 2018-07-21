c46e1f49e7d4abfb6f0f82c0aac7b7317a82d5a4
Noah Apthorpe <apthorpe@princeton.edu>
2018-07-20 20:31:45 -0400

    correct quotes

--- package_install/package_install.sh
+++ package_install/package_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ “$OSTYPE” == “linux-gnu” ]]; then
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # ...
     curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > anaconda_install.sh
-elif [[ “$OSTYPE” == “Darwin”* ]]; then
+elif [[ "$OSTYPE" == "Darwin"* ]]; then
     # Mac OSX
     curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > anaconda_install.sh
-elif [[ “$OSTYPE” == “msys” ]]; then
+elif [[ "$OSTYPE" == "msys" ]]; then
     # Windows git bash
     curl https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe > anaconda_install.sh
 else
